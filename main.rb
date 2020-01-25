aa59cf7f1a33319677b8785c4810ed89ee3c8535
nntakuya <uv.nkmt.yii@gmail.com>
2020-01-25 13:53:29 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  'how are you?'
+end
+
+get '/path/to' do
+  "this is [/path/to]"
+end
+
+# ここから
+get '/hello/*' do |name|
+  "hello #{name}. how are you?"
+end
+
+get '/erb_template_page' do
+  erb :erb_template_page
+end
